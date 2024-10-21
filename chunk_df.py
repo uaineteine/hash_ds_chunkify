2041b61bb135ed1d891423f8e38bc310967341ff
Daniel <39141263+uaineteine@users.noreply.github.com>
2024-10-21 23:41:05 +1100

    Update chunk_df.py

--- chunk_df.py
+++ chunk_df.py
@@ -1,6 +1,7 @@
 import os
 import pandas as pd
 import pyarrow
+from hash_ds import read_file
 print("[hash_ds_chunkify::chunk_df.py] defining functions")
 
 def chunkify_df(df, chunk_size, output_dir):
